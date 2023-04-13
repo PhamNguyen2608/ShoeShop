796d3eb08b2cd4d72e0eb3c076373f11d5fda83e
Pham Van Nguyen <89583094+PhamNguyen2608@users.noreply.github.com>
2023-04-13 22:48:16 +0700

    Update server.js

--- server/server.js
+++ server/server.js
@@ -8,7 +8,12 @@ import userRouter from "./Routes/UserRoutes.js";
 import orderRouter from "./Routes/orderRoutes.js";
 import discountRoute from "./Routes/discountRoutes.js";
 import categoriesRoute from "./Routes/categoriesRoutes.js";
+import { fileURLToPath } from "url";
+import { dirname } from "path";
 import path from "path";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 dotenv.config();
 connectDatabase();
@@ -16,12 +21,12 @@ const app = express();
 app.use(express.json());
 
 // Serve static files from the 'client' directory
-app.use(express.static(path.join(__dirname, "..", "client frontend")));
+app.use(express.static(path.join(__dirname, "..", "client")));
 
 // Handle other routes
 app.get("*", (req, res, next) => {
   if (req.originalUrl.startsWith("/api")) return next();
-  res.sendFile(path.join(__dirname, "..", "client frontend", "index.html"));
+  res.sendFile(path.join(__dirname, "..", "client", "index.html"));
 });
 
 // API
